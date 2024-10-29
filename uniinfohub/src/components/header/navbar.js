b5582960dc4bb62323f30c4860bb8bf874f4e87c
shwakib <sifat6460@gmail.com>
2024-10-29 03:00:28 -0400

    navbar complete.

--- uniinfohub/src/components/header/navbar.js
+++ uniinfohub/src/components/header/navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../assets/image/Uniinfohub_logo.png'; // Replace with the actual path to your logo image
+import logo from '../../assets/image/Uniinfohub_logo.png';
 import '../../css/navbar.css';
 
 function Navbar() {
@@ -12,25 +12,37 @@ function Navbar() {
         <li>
           <a href="#health">HEALTH</a>
           <ul className="dropdown">
-            {/* Add dropdown items here */}
+            <li><a href="#gss-provider">GSS & Provider - Green Shield Canada</a></li>
+            <li><a href="#get-gsc-card">How to get GSC Card</a></li>
+            <li><a href="#before-getting-gsc">Before getting GSC Card</a></li>
+            <li><a href="#opt-out-health">Opt out from Health Insurance</a></li>
           </ul>
         </li>
         <li>
           <a href="#transportation">TRANSPORTATION</a>
           <ul className="dropdown">
-            {/* Add dropdown items here */}
+            <li><a href="#before-buss-pass">Before Getting Bus Pass</a></li>
+            <li><a href="#buss-pass-and-validity">Get a Bus Pass & Validity</a></li>
+            <li><a href="#bus-stop-list">Check Bus Schedule & Bus Stop List</a></li>
+            <li><a href="#refill-bus-pass">Refill Bus Pass</a></li>
           </ul>
         </li>
         <li>
           <a href="#library">LIBRARY & RESEARCH</a>
           <ul className="dropdown">
-            {/* Add dropdown items here */}
+            <li><a href="#how-to-print">How & Where to Print</a></li>
+            <li><a href="#laod-uwincard">Load UWINcard</a></li>
+            <li><a href="#book-room">Book a Room in the Leddy Library & Amenities</a></li>
+            <li><a href="#search-journal">Look for Research Papers in OMNI Library</a></li>
           </ul>
         </li>
         <li>
           <a href="#student-services">STUDENT SERVICES</a>
           <ul className="dropdown">
-            {/* Add dropdown items here */}
+            <li><a href="#sin-clinic">SIN Clinic & Service Canada</a></li>
+            <li><a href="#driving-license-basic">Getting Driving License, Abstract & G1</a></li>
+            <li><a href="#accomodation-dorm">Accommodation in University’s Dormitory</a></li>
+            <li><a href="#renting-home">Renting in Canada</a></li>
           </ul>
         </li>
         <li>
