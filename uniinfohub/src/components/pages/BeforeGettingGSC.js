684cb906031daf1997ce78abdde1d7786fdc2b97
mohamm8z <mohamm8z@uwindsor.ca>
2024-11-06 20:03:14 -0500

    Layout of gcc provider screen created

---
+++ uniinfohub/src/components/pages/BeforeGettingGSC.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function BeforeGettingGSC() {
+  return <h1>Before getting GSC Card</h1>;
+}
+
+export default BeforeGettingGSC;
---
+++ uniinfohub/src/components/pages/OptOutHealth.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function OptOutHealth() {
+  return <h1>Opt out from Health Insurance</h1>;
+}
+
+export default OptOutHealth;
