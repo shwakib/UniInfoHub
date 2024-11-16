0a6989ce8e879ac568e917c9f74b3042a1f82222
Sifat Hasan Wakib <sifat6460@gmail.com>
2024-11-15 19:53:52 -0500

    Update index.js

--- uniinfohub/src/index.js
+++ uniinfohub/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Landing from '../src/components/pages/landing/Landing'
+// import Landing from '../src/components/pages/landing/Landing'
 import Transport from '../src/components/pages/transport/Transport'
 // import BeforeGettingBusPass from '../src/components/pages/transport/BeforeGettingBusPass'
 import GetBusPass from '../src/components/pages/transport/GetBusPass'
@@ -12,12 +13,6 @@ import Studentservices from '../src/components/pages/services/Studentservices'
 import Rentingrooms from '../src/components/pages/services/Rentingrooms' 
 import Drivinglicense from '../src/components/pages/services/Drivinglicense' 
 import Accommodation from '../src/components/pages/services/Accommodation' 
-
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 
 const router = createBrowserRouter([
   {
