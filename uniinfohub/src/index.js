d9a162f9a9deddce7ee82028c2b62eaf9a09499f
Sifat Hasan Wakib <sifat6460@gmail.com>
2024-11-15 20:01:46 -0500

    Update index.js

--- uniinfohub/src/index.js
+++ uniinfohub/src/index.js
@@ -15,45 +15,7 @@ import Drivinglicense from '../src/components/pages/services/Drivinglicense'
 import Accommodation from '../src/components/pages/services/Accommodation' 
 
 const router = createBrowserRouter([
-  {
-    path: "/transport",
-    element: <Transport />
-  },
-  // {
-  //   path: "/transport/beforegettingbuspass",
-  //   element: <BeforeGettingBusPass />
-
-  // },
-  {
-    path: "/transport/getbuspass",
-    element: <GetBusPass />
-
-  },
-  {
-    path: "/transport/refillbuspass",
-    element: <RefillBusPass />
-
-  },
-  {
-    path: "/services/Studentservices",
-    element: <Studentservices />
-
-  },
-  {
-    path: "/services/Accommodation",
-    element: <Accommodation />
-
-  },
-  {
-    path: "/services/Drivinglicense",
-    element: <Drivinglicense />
-
-  },
-  {
-    path: "/services/Rentingrooms",
-    element: <Rentingrooms />
-
-  },
+  
 
 ]);
 
