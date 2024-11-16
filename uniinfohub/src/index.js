93801bacb3b5680154c149bb3fd0e1bf02cca30d
Sifat Hasan Wakib <sifat6460@gmail.com>
2024-11-15 20:05:24 -0500

    Update index.js

--- uniinfohub/src/index.js
+++ uniinfohub/src/index.js
@@ -4,30 +4,50 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import Landing from '../src/components/pages/landing/Landing'
-import Transport from '../src/components/pages/transport/Transport'
-// import BeforeGettingBusPass from '../src/components/pages/transport/BeforeGettingBusPass'
-import GetBusPass from '../src/components/pages/transport/GetBusPass'
-import RefillBusPass from '../src/components/pages/transport/RefillBusPass'
-import Studentservices from '../src/components/pages/services/Studentservices' 
-import Rentingrooms from '../src/components/pages/services/Rentingrooms' 
-import Drivinglicense from '../src/components/pages/services/Drivinglicense' 
-import Accommodation from '../src/components/pages/services/Accommodation' 
+import Houserent from './components/pages/rent/Houserent';
+import Transport from '../src/components/pages/landing/Transport';
+import GSSProvider from '../src/components/pages/Health/GSSProvider';
+import GetGSC from '../src/components/pages/Health/GetGSC';
+import BeforeGettingGSC from '../src/components/pages/Health/BeforeGettingGSC';
+import OptOutHealth from '../src/components/pages/Health/OptOutHealth';
+import HowAndWhereToPrint from '../src/components/pages/LibraryResearch/HowAndWhereToPrint';
+import LoadUwinCard from '../src/components/pages/LibraryResearch/LoadUwinCard';
+import BookRoom from '../src/components/pages/LibraryResearch/BookRoom';
+import SearchJournal from '../src/components/pages/LibraryResearch/SearchJournal';
+import GetBusPass from '../src/components/pages/transport/GetBusPass';
+import RefillBusPass from '../src/components/pages/transport/RefillBusPass';
+import Studentservices from '../src/components/pages/services/Studentservices';
+import Rentingrooms from '../src/components/pages/services/Rentingrooms';
+import Drivinglicense from '../src/components/pages/services/Drivinglicense'; 
+import Accommodation from '../src/components/pages/services/Accommodation';
 
 const router = createBrowserRouter([
-  
-
+  {
+    path: "/",
+    element: <App />,
+  },
+  { path: "/transport", element: <Transport /> },
+  { path: "/gss-provider", element: <GSSProvider /> },
+  { path: "/get-gsc-card", element: <GetGSC /> },
+  { path: "/before-getting-gsc", element: <BeforeGettingGSC /> },
+  { path: "/opt-out-health", element: <OptOutHealth /> },
+  { path: "/rent", element: <Houserent /> },
+  { path: "/how-to-print", element: <HowAndWhereToPrint /> },
+  { path: "/load-uwincard", element: <LoadUwinCard /> },
+  { path: "/book-room", element: <BookRoom /> },
+  { path: "/transport/getbuspass", element: <GetBusPass /> },
+  { path: "/transport/refillbuspass", element: <RefillBusPass /> },
+  { path: "/services/Studentservices", element: <Studentservices /> },
+  { path: "/services/Accommodation", element: <Accommodation /> },
+  { path: "/services/Drivinglicense", element: <Drivinglicense /> },
+  { path: "/search-journal", element: <SearchJournal /> },
 ]);
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
