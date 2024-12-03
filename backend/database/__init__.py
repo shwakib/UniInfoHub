b49fda64d9cc0dc1b59eca520563128abb039dba
shwakib <sifat6460@gmail.com>
2024-12-03 10:17:29 -0500

    update

--- backend/database/__init__.py
+++ backend/database/__init__.py
@@ -6,5 +6,5 @@ def init_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:password123@localhost/appointments'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-    # with app.app_context():
-    #     db.create_all()
+    with app.app_context():
+        db.create_all()
